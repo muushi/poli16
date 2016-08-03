dda60b6d424b3aed040968517df26125ee9faf40
Niklas Sivaro <niklas.sivaro@aalto.fi>
2016-08-03 15:48:14 +0300

    add logos, start on styling

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Polin Appro 2016 website
 Static website for the Polin Appro 2016 event
+
+http://devpro.it/pixelfont/ - js
--- static/js/page.js
+++ static/js/page.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9'],
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9', 'page10'],
         menu: "#navigation",
         recordHistory: false
     });
 });
+
+function localize(lang) {
+  $("[data-localize]").localize("site", {
+      pathPrefix: "static/localization",
+      language: lang
+  });
+};
