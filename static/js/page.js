c57af1ffe7c3f5ed97fa437aab17527a82338d3b
Niklas Sivaro <niklas.sivaro@aalto.fi>
2016-10-03 14:15:08 +0300

    Some mobile fixes, flag addition, etc.

--- static/js/page.js
+++ static/js/page.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
+        //12 anchors now
         anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9', 'page10'],
         menu: "#navigation",
+        lockAnchors: true,
         recordHistory: false
+        //autoScrolling: false
     });
-    $.fn.fullpage.setAutoScrolling(false);
+    //$.fn.fullpage.setAutoScrolling(false);
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || (/*@cc_on!@*/false || !!document.documentMode)){
         $.fn.fullpage.setAutoScrolling(false);
     }
-    L.mapbox.accessToken = 'pk.eyJ1IjoibXV1c2hpIiwiYSI6ImNpcmV3ZHQ0ZDAwMTdpZGx5bTQwNTRuc3YifQ.rkg_L29WePLjrbYEspG2XA';
-    var map = L.mapbox.map('map', 'muushi.118bcbc5');
+    //placeholder map for test purposes
+    /*L.mapbox.accessToken = 'pk.eyJ1IjoibXV1c2hpIiwiYSI6ImNpcmV3ZHQ0ZDAwMTdpZGx5bTQwNTRuc3YifQ.rkg_L29WePLjrbYEspG2XA';
+    var map = L.mapbox.map('map', 'muushi.118bcbc5');*/
 });
 
 function localize(lang) {
